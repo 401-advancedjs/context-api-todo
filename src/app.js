925a1e89d534a0e6e4b4a432aac512dbb65f7beb
Melissa Stock <melissastock44@gmail.com>
2019-06-25 16:06:02 -0700

    refactored using context

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import ToDo from './components/todo/todo.js';
+import RefactorContext from './components/context/context.js';
 
 export default class App extends React.Component {
   render() {
     return (
       <>
+      <RefactorContext>
         <ToDo />
+      </RefactorContext>
       </>
     );
   }
